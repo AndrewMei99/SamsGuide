8c030b3123cc65f38f1ea6326f135c528d22882d
EC2 Default User <ec2-user@ip-172-31-94-74.ec2.internal>
2019-08-07 22:37:04 +0000

    Folder with results page and few changes to Homepage.py

--- HomePage.py
+++ HomePage.py
@@ -1,6 +1,7 @@
 import webapp2
 import jinja2
 import os
+import logging 
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -8,12 +9,27 @@ the_jinja_env = jinja2.Environment(
     autoescape=True)
     
     
+class SearchResult(webapp2.RequestHandler):
+    def post(self):
+        #results_template = the_jinja_env.get_template('/results.html')
+        user_input = self.request.get('search')
+        
+        
+search = ["Drug alcohol", "health care", "child care"]
+
+for i in search:
+    if i == user_input:
+        self.response.write(results_template)
+
+
     
+
+    
+
     
     
     
     
 app = webapp2.WSGIApplication([
-    ('/', EnterInfoHandler),
-    ('/showmeme', ShowMemeHandler),
+    ('/search', EnterInfoHandler),
 ], debug=True)
